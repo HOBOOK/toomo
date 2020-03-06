20d279ae4a5142e87a7f1adfaf52233596aaa63f
Hobook <37508173+HOBOOK@users.noreply.github.com>
2020-03-06 21:31:31 +0900

    메모 모델 바인드

---
+++ src/main/java/com/hobook/tomo/model/Memo.java
@@ -0,0 +1,42 @@
+package com.hobook.tomo.model;
+
+public class Memo {
+    private String id;
+    private String content;
+    private String dateOfCreate;
+    private int state; //0:보여짐, 1:가려짐, 2>= 제거
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDateOfCreate() {
+        return dateOfCreate;
+    }
+
+    public void setDateOfCreate(String dateOfCreate) {
+        this.dateOfCreate = dateOfCreate;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+
+}
