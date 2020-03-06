17d492d473b1500fa9a2905bd12485ac13d28d71
Hobook <37508173+HOBOOK@users.noreply.github.com>
2020-03-06 11:23:26 +0900

    웹 프로젝트 시작

---
+++ src/main/java/com/hobook/tomo/TomoApplication.java
@@ -0,0 +1,12 @@
+package com.hobook.tomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TomoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hobook/tomo/TomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hobook.tomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
