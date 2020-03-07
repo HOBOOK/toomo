961a16e20a82d78bab3825904ee629d84a95d7d7
Hobook <37508173+HOBOOK@users.noreply.github.com>
2020-03-08 02:50:11 +0900

    하이라이트 텍스트 볼드체 기능 추가 2

--- src/main/resources/static/js/memo.js
+++ src/main/resources/static/js/memo.js
@@ -98,8 +98,12 @@ app.controller('memoController', function ($scope, $compile) {
     $scope.setBoldText = function(memo) {
         $scope.config.eventDelay = 1;
         var text = "";
+
         if (typeof window.getSelection != "undefined") {
             var sel = window.getSelection();
+            var start = sel.anchorOffset;
+            var end = sel.focusOffset;
+            console.log(start + " > " + end);
             if (sel.rangeCount) {
                 var container = document.createElement("div");
                 for (var i = 0, len = sel.rangeCount; i < len; ++i) {
@@ -114,50 +118,66 @@ app.controller('memoController', function ($scope, $compile) {
         }
         if(text==="")
             return;
+
         var target = document.getElementById("memo_context_"+memo.id);
         var context = target.innerHTML;
         var before = "";
         var after = "";
 
-        if(text.search('<span')!== -1){
-            if(text.charAt(0)==='<'){
-                before = context.slice(0, context.indexOf(text));
-                after = context.slice(context.indexOf(text)+text.length, context.length);
-                if(before.lastIndexOf('<span')!==-1){
-                    text = text.replace(text.substring(text.indexOf('<span'),text.indexOf('</span>')+7),"");
-                    text = before.substring(before.lastIndexOf('<span'), before.lastIndexOf('</span>')+7) + text;
-                    before = before.substring(0, before.lastIndexOf('<span'));
-                    after = context.slice(context.indexOf(text)+text.length, context.length);
-                }
-            }else{
-                var tempText = text.substring(0,text.indexOf('</span>'));
-                before = context.slice(0, context.indexOf(tempText));
-                after = context.slice(context.indexOf(tempText)+tempText.length, context.length);
-                if(after.indexOf('</span>')!==-1){
-                    text = text.replace(text.substring(text.indexOf('</span>'),text.indexOf('</span>')+7),"");
-                    text = text + after.substring(0,after.indexOf('</span>'+7));
-                    after = after.substring(after.indexOf('</span>'+7));
-                }
-            }
-            while(text.search('<span') !== -1){
-                text = text.replace(text.substring(text.indexOf('<span'),text.indexOf('>')+1),"");
-            }
-            while(text.search('</span>')!==-1){
-                text = text.replace(text.substring(text.indexOf('</span>'),text.indexOf('</span>')+7),"");
-            }
-
-            text = '<span class=\'context_bold\'\>' + text + '\<\/span\>';
-
-
-        }else{
+        if(text.charAt(0)==='<' && text.charAt(text.length-1)==='>'){
             before = context.slice(0, context.indexOf(text));
             after = context.slice(context.indexOf(text)+text.length, context.length);
-            if(after.substring(0,7)==='</span>'){
-                before = before.substring(0,before.lastIndexOf('<span'));
-                after = after.substring(7,after.length);
+            text = text.replace(/(<([^>]+)>)/ig,"");
+            text = '<span class=\'context_bold\'\>' + text + '\<\/span\>';
+        }
+        else if(text.search('<span')!== -1){
+            var matchCount = text.split('<span').length - 1;
+            if(matchCount>1){
+                var tempText = text.substring(0,text.lastIndexOf('</span>'));
+                before = context.slice(0, context.indexOf(tempText));
+                after = context.slice(context.indexOf(tempText)+text.length, context.length);
+            }else{
+                var tempText = text.substring(0,text.indexOf('</span>'));
+                if(text.substring(0,5)==='<span'){
+                    before = context.slice(0, context.indexOf(tempText));
+                    after = context.slice(context.indexOf(tempText)+text.length, context.length);
+                    if(before.lastIndexOf('<span')!==-1){
+                        text = text.replace(text.substring(text.indexOf('<span'),text.indexOf('</span>')+7),"");
+                        text = before.substring(before.lastIndexOf('<span'), before.lastIndexOf('</span>')+7) + text;
+                        before = before.substring(0, before.lastIndexOf('<span'));
+                        after = context.slice(context.indexOf(text)+text.length, context.length);
+                    }
+                }else if(text.substring(text.length-7,text.length)==='</span>'){
+                    before = context.slice(0, context.indexOf(tempText));
+                    after = context.slice(context.indexOf(tempText)+tempText.length, context.length);
+                    if(after.indexOf('</span>')!==-1){
+                        text = text.replace(text.substring(text.indexOf('</span>'),text.indexOf('</span>')+7),"");
+                        text = text + after.substring(0,after.indexOf('</span>')+7);
+                        after = after.substring(after.indexOf('</span>')+7,after.length);
+                    }
+                }
             }
             text = text.replace(/(<([^>]+)>)/ig,"");
             text = '<span class=\'context_bold\'\>' + text + '\<\/span\>';
+            console.log(text);
+        }else{
+            before = context.slice(0, context.indexOf(text));
+            after = context.slice(context.indexOf(text)+text.length, context.length);
+            if(before.charAt(before.length-1)==='>'&&after.indexOf('</span>')!==-1){
+                before = before.substring(0,before.lastIndexOf('<span'));
+                if(after.substring(0,7)==='</span>'){
+                    after = after.substring(7);
+                }else{
+                    after = '<span class=\'context_bold\'\>' + after;
+                }
+            } else if(after.substring(0,7)==='</span>'&&before.lastIndexOf('<span')!==-1){
+                after = after.substring(7);
+                before = before + '</span>';
+            }
+            else{
+                text = text.replace(/(<([^>]+)>)/ig,"");
+                text = '<span class=\'context_bold\'\>' + text + '\<\/span\>';
+            }
         }
 
         var retText = before + text + after;
