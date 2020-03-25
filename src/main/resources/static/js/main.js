ec0de823c93e8fccb10a4a1d23fb8a11bad4f483
Hobook <37508173+HOBOOK@users.noreply.github.com>
2020-03-25 18:52:39 +0900

    우측 내비게이션 패널추가

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -7,4 +7,44 @@ $(window).on('load',(function() {
         documentTouchScroll : false,
         autoDraggerLength: false
     });
+    var toggleVal = getCookie('toggleBar');
+    if(toggleVal==null || toggleVal=='false'){
+        $('.mybar').css('width','0px');
+        $('.mybar_toggle').css('box-shadow','1px 2px 3px 1px rgba(0,0,0,0.2)');
+        $('.container_main').css('width','calc(100% - 80px)');
+    }else{
+        $('.mybar').css('width','250px');
+        $('.mybar_toggle').css('box-shadow','none');
+        $('.container_main').css('width','calc(100% - 330px)');
+    }
 }));
+
+function toggleBar() {
+    var val = getCookie('toggleBar');
+    if(val==null || val=='false'){
+        setCookie('toggleBar', true, 7);
+        val = true;
+    }else{
+        setCookie('toggleBar', false, 7);
+        val = false;
+    }
+    if(val){
+        $('.mybar').css('width','250px');
+        $('.mybar_toggle').css('box-shadow','none');
+        $('.container_main').css('width','calc(100% - 330px)');
+    }else{
+        $('.mybar').css('width','0px');
+        $('.mybar_toggle').css('box-shadow','1px 2px 3px 1px rgba(0,0,0,0.2)');
+        $('.container_main').css('width','calc(100% - 80px)');
+    }
+}
+function setCookie(name, value, exp){
+    var date = new Date();
+    date.setTime(date.getTime() + exp*24*60*1000);
+    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + ';path=/';
+}
+
+function getCookie(name){
+    var value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+    return value ? value[2] : null;
+}
